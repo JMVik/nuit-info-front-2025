402a93f16f5b64210af7a4e7b894c420770ca3aa
JMVik <180157747+JMVik@users.noreply.github.com>
2025-12-05 01:44:07 +0100

    Linux + snake

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'linux',
+    title: 'NuitInfo - Linux',
+    loadComponent: () => import('./linux/linux.component').then(c => c.LinuxComponent),
+  },
+  {
+    path: 'snake',
+    title: 'NuitInfo - Snake',
+    loadComponent: () => import('./linux/snake/snake.component').then(c => c.SnakeComponent)
+  }
+];
